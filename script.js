b07bb24a1ffe6c5a848e97ee4ea1159703a1919a
SantRocha <128643723+SantRocha@users.noreply.github.com>
2023-09-20 12:33:10 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Função para ajustar a opacidade e escala com base na posição da barra de rolagem
+function adjustOpacityAndScaleOnScroll() {
+    var conteudos = document.querySelectorAll('.conteudo');
+  
+    // Obtém a posição vertical da barra de rolagem
+    var scrollY = window.scrollY || window.pageYOffset;
+  
+    conteudos.forEach(function (conteudo, index) {
+      var conteudoTop = conteudo.getBoundingClientRect().top;
+      var conteudoHeight = conteudo.offsetHeight;
+  
+      // Calcule a opacidade com base na posição do elemento na tela
+      var opacity = 1 - (conteudoTop - 300 - scrollY) / conteudoHeight;
+  
+      // Garanta que a opacidade esteja no intervalo de 0 a 1
+      opacity = Math.min(1, Math.max(0, opacity));
+  
+      // Calcule o atraso com base no índice (aumente o atraso com base no índice)
+      var delay = 600 * index; // Atraso de 200ms multiplicado pelo índice
+  
+      // Aplique a opacidade aos elementos com atraso
+      setTimeout(function () {
+        conteudo.style.opacity = opacity;
+      }, delay);
+  
+      // Aplique a escala com atraso correspondente
+      setTimeout(function () {
+        conteudo.style.transform = 'scale(' + opacity + ')';
+      }, delay);
+    });
+  }
+  
+  // Adiciona um ouvinte de evento de rolagem para chamar a função de ajuste de opacidade e escala
+  window.addEventListener('scroll', adjustOpacityAndScaleOnScroll);
+  
+  // Chama a função de ajuste de opacidade e escala inicialmente ao carregar a página
+  adjustOpacityAndScaleOnScroll();
+  
